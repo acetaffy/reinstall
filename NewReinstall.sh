8f3200228dbe443437ce1e80809151dde0998cea
acetaffy <92644683+acetaffy@users.noreply.github.com>
2023-11-09 23:29:12 +0800

    Update NewReinstall.sh

--- NewReinstall.sh
+++ NewReinstall.sh
@@ -154,7 +154,7 @@ function Start() {
   fi
 
   if [[ "$isCN" == '1' ]]; then
-   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://cdn.jsdelivr.net/gh/fcurrk/reinstall@master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
+   wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://gcore.jsdelivr.net/gh/fcurrk/reinstall@master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
   else 
    wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://raw.githubusercontent.com/fcurrk/reinstall/master/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
   fi
